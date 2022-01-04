f327eef4103fc66763ee9cb195e731824d97a24a
benlittlenz <benlittle53@gmail.com>
2022-01-04 13:31:47 +1300

    Added basic button api and stories file

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,13 @@
+import { Story, Meta } from '@storybook/react';
+import React from 'react';
+
+import { Button } from './Button';
+
+export default {
+  title: 'Example/Button',
+  component: Button,
+} as Meta;
+
+const Template: Story = (args) => <Button {...args}>MyButton</Button>;
+
+export const Default = Template.bind({});
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Button = React.forwardRef<HTMLButtonElement, React.ComponentPropsWithoutRef<'button'>>(
+  ({ children, ...props }, ref) => {
+    return (
+      <button ref={ref} type="button" {...props}>
+        {children}
+      </button>
+    );
+  }
+);
+
+Button.displayName = 'Button';
