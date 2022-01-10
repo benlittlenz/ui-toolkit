1a85ac94c7a8a4505ce957546fa15591291d753f
benlittlenz <benlittle53@gmail.com>
2022-01-10 20:39:16 +1300

    usedisclosure hook tests

--- src/hooks/__tests__/useDisclosure.test.ts
+++ src/hooks/__tests__/useDisclosure.test.ts
@@ -13,3 +13,39 @@ test('it should open the state', () => {
 
   expect(result.current.isOpen).toBe(true);
 });
+
+test('it should close the state', () => {
+  const { result } = renderHook(() => useDisclosure());
+
+  expect(result.current.isOpen).toBe(false);
+
+  act(() => {
+    result.current.close();
+  });
+
+  expect(result.current.isOpen).toBe(false);
+});
+
+test('it should toggle the state', () => {
+  const { result } = renderHook(() => useDisclosure());
+
+  expect(result.current.isOpen).toBe(false);
+
+  act(() => {
+    result.current.toggle();
+  });
+
+  expect(result.current.isOpen).toBe(true);
+});
+
+test('it should define initial state', () => {
+  const { result } = renderHook(() => useDisclosure(true));
+
+  expect(result.current.isOpen).toBe(true);
+
+  act(() => {
+    result.current.toggle();
+  });
+
+  expect(result.current.isOpen).toBe(false);
+});
