12101d5dec2beee8d7735f81bcde5407311ed9c8
benlittlenz <benlittle53@gmail.com>
2022-01-11 10:21:53 +1300

    Feat: Added table body styling

---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Table = styled.div`
+  position: relative;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  max-width: 100%;
+`;
+
+export default Table;
