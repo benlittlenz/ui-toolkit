f0d94a153b86f2257a91690abd1d46b99abbf506
benlittlenz <benlittle53@gmail.com>
2022-01-13 15:46:25 +1300

    added test for onSelectedRowsChange method

--- src/components/Table/__tests__/Datatable.test.tsx
+++ src/components/Table/__tests__/Datatable.test.tsx
@@ -1,4 +1,5 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import { DataTable } from '../Datatable';
 
@@ -34,4 +35,70 @@ test('it should render correctly with the selector function', () => {
 
   const { container } = render(<DataTable data={data} columns={columns} />);
   expect(container.firstChild).toMatchSnapshot();
+});
+
+describe('onSelectedRowsChange method', () => {
+  test('it should call onSelectedRowsChange with correct values when select all has been selected', () => {
+    const mock = dataMock();
+    const updatedMock = jest.fn();
+
+    render(
+      <DataTable
+        data={mock.data}
+        columns={mock.columns}
+        selectableRows
+        onSelectedRowsChange={updatedMock}
+      />
+    );
+
+    userEvent.click(screen.getByRole('checkbox', { name: 'select-all-rows' }));
+
+    expect(updatedMock).toBeCalledWith({
+      allSelected: true,
+      selectedCount: 2,
+      selectedRows: mock.data,
+    });
+  });
+  test('it should call onSelectedRowsChange with correct values when all rows have been selected', () => {
+    const mock = dataMock();
+    const onSelectedRowsChange = jest.fn();
+
+    render(
+      <DataTable
+        data={mock.data}
+        columns={mock.columns}
+        selectableRows
+        onSelectedRowsChange={onSelectedRowsChange}
+      />
+    );
+
+    userEvent.click(screen.getByRole('checkbox', { name: 'select-row-1' }));
+
+    expect(onSelectedRowsChange).toBeCalledWith({
+      allSelected: false,
+      selectedCount: 1,
+      selectedRows: [
+        {
+          id: 1,
+          name: 'Apple',
+        },
+      ],
+    });
+    userEvent.click(screen.getByRole('checkbox', { name: 'select-row-2' }));
+
+    expect(onSelectedRowsChange).toBeCalledWith({
+      allSelected: true,
+      selectedCount: 2,
+      selectedRows: [
+        {
+          id: 2,
+          name: 'Zuchinni',
+        },
+        {
+          id: 1,
+          name: 'Apple',
+        },
+      ],
+    });
+  });
 });
